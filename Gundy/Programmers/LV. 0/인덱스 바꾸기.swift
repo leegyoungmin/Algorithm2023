7f9adba56fc99b6425255f89bb4b75ac83222f0e
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-03-29 18:12:44 +0900

    Update 인덱스 바꾸기.swift

--- Gundy/Programmers/LV. 0/인덱스 바꾸기.swift
+++ Gundy/Programmers/LV. 0/인덱스 바꾸기.swift
@@ -1,7 +1,5 @@
 func solution(_ my_string:String, _ num1:Int, _ num2:Int) -> String {
     var result: [String] = my_string.map({ String($0) })
-    let text: String = result[num1]
-    result[num1] = result[num2]
-    result[num2] = text
+    result.swapAt(num1, num2)
     return result.joined()
 }
