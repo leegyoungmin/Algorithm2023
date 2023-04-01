f50525b7078ae60d75196880ff5066752d077e53
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-04-01 19:47:34 +0900

    Create 미로 탐색.swift

---
+++ Gundy/BAEKJOON/BFS/미로 탐색.swift
@@ -0,0 +1,90 @@
+extension Array {
+    subscript(safe index: Int) -> Element? {
+        guard self.count > index && index >= 0 else {
+            return nil
+        }
+        return self[index]
+    }
+}
+
+struct Queue<T> {
+    private var left: [T] = []
+    private var right: [T] = []
+    var isEmpty: Bool {
+        return left.isEmpty && right.isEmpty
+    }
+    
+    mutating func enqueue(_ value: T) {
+        right.append(value)
+    }
+    
+    mutating func dequeue() -> T? {
+        if left.isEmpty {
+            left = right.reversed()
+            right = []
+        }
+        return left.popLast()
+    }
+}
+
+func solution() {
+    let size: [Int] = readLine()!.split(separator: " ").compactMap({ Int($0) })
+    var maze: [[Int]] = []
+    for _ in 1...size[0] {
+        maze.append(readLine()!.compactMap({ Int(String($0)) }))
+    }
+    var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: size[1]), count: size[0])
+    var distance: [[Int]] = Array(repeating: Array(repeating: 0, count: size[1]), count: size[0])
+    var needVisit: Queue<[Int]> = Queue()
+    distance[0][0] = 1
+    for x in 0...size[0]-1 {
+        for y in 0...size[1]-1 {
+            if visited[x][y] {
+                continue
+            }
+            needVisit.enqueue([x, y])
+            while needVisit.isEmpty == false {
+                let point = needVisit.dequeue()!,
+                    x = point[0],
+                    y = point[1]
+                if visited[x][y] {
+                    continue
+                }
+                visited[x][y] = true
+                guard maze[x][y] == 1 else {
+                    continue
+                }
+                if visited[x][safe: y-1] == false {
+                    needVisit.enqueue([x, y-1])
+                    if distance[x][y-1] == 0 {
+                        distance[x][y-1] = distance[x][y] + 1
+                    }
+                }
+                if let down = visited[x][safe: y+1],
+                   down == false {
+                    needVisit.enqueue([x, y+1])
+                    if distance[x][y+1] == 0 {
+                        distance[x][y+1] = distance[x][y] + 1
+                    }
+                }
+                if let left = visited[safe: x-1]?[y],
+                   left == false {
+                    needVisit.enqueue([x-1, y])
+                    if distance[x-1][y] == 0 {
+                        distance[x-1][y] = distance[x][y] + 1
+                    }
+                }
+                if let right = visited[safe: x+1]?[y],
+                   right == false {
+                    needVisit.enqueue([x+1, y])
+                    if distance[x+1][y] == 0 {
+                        distance[x+1][y] = distance[x][y] + 1
+                    }
+                }
+            }
+        }
+    }
+    print(distance[size[0]-1][size[1]-1])
+}
+
+solution()
