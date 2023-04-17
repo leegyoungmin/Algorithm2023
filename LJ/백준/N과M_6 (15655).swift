18b65b47504616ee27f5f6fd8516037bf2dadbb4
LJ <swtw72@gmail.com>
2023-04-17 13:15:33 +0900

    Create N과M_6 (15655).swift

---
+++ LJ/백준/N과M_6 (15655).swift
@@ -0,0 +1,26 @@
+//N과 M (6)
+//https://www.acmicpc.net/problem/15655
+
+let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
+let N = NM[0], M = NM[1]
+let numbers = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted(by: <)
+var array = [Int]()
+
+recursion(M)
+
+func recursion(_ depth: Int) {
+    if depth == 0 { return }
+    for i in 0..<N {
+        if array.contains(numbers[i]) { continue }
+        if array.isEmpty == false && array.last! > numbers[i] {
+            continue
+        }
+        array.append(numbers[i])
+        if array.count == M {
+            print(array.map{ String($0) }.joined(separator: " "))
+        } else if depth > 0 {
+            recursion(depth-1)
+        }
+        array.popLast()
+    }
+}
