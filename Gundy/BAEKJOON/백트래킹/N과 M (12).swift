953d5c2027b8fe72e863ecc522348f9f70274d74
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-05-09 22:17:16 +0900

    Create N과 M (12).swift

---
+++ Gundy/BAEKJOON/백트래킹/N과 M (12).swift
@@ -0,0 +1,18 @@
+let targetCount = readLine()!.split(separator: " ").compactMap({ Int($0) })[1]
+let numbers = readLine()!.split(separator: " ").compactMap({ Int($0) }).sorted()
+var result = Set<[Int]>()
+
+func backTracking(_ index: Int,  _ indices: [Int]) {
+    if indices.count == targetCount {
+        let numberList = indices.map({ numbers[$0] })
+        guard result.contains(numberList) == false else { return }
+        result.insert(numberList)
+        print(numberList.map({ String($0) }).joined(separator: " "))
+        return
+    }
+    for nextIndex in index..<numbers.count {
+        backTracking(nextIndex, indices + [nextIndex])
+    }
+}
+
+backTracking(0, [])
