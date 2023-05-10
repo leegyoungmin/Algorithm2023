1a648c8895120f7957eadaaa62efc0c4ccccaf59
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-05-10 16:54:04 +0900

    Create 연속합.swift

---
+++ Gundy/BAEKJOON/다이나믹 프로그래밍/연속합.swift
@@ -0,0 +1,21 @@
+let countOfNumber = Int(readLine()!)!
+let numbers = readLine()!.split(separator: " ").compactMap({ Int($0) })
+var result = numbers[0]
+var partialResult = numbers[0]
+
+if numbers.filter({ $0 > 0 }).isEmpty {
+    result = numbers.sorted().last!
+} else {
+    for index in 1..<countOfNumber {
+        if partialResult < 0 {
+            partialResult = numbers[index]
+        } else {
+            partialResult += numbers[index]
+            if partialResult > result {
+                result = partialResult
+            }
+        }
+    }
+}
+
+print(result)
