822393ee84bfb207a9f4b647a88d0209bb81263d
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-05-10 16:56:14 +0900

    Update 연속합.swift

--- Gundy/BAEKJOON/다이나믹 프로그래밍/연속합.swift
+++ Gundy/BAEKJOON/다이나믹 프로그래밍/연속합.swift
@@ -3,18 +3,14 @@ let numbers = readLine()!.split(separator: " ").compactMap({ Int($0) })
 var result = numbers[0]
 var partialResult = numbers[0]
 
-if numbers.filter({ $0 > 0 }).isEmpty {
-    result = numbers.sorted().last!
-} else {
-    for index in 1..<countOfNumber {
-        if partialResult < 0 {
-            partialResult = numbers[index]
-        } else {
-            partialResult += numbers[index]
-            if partialResult > result {
-                result = partialResult
-            }
-        }
+for index in 1..<countOfNumber {
+    if partialResult < 0 {
+        partialResult = numbers[index]
+    } else {
+        partialResult += numbers[index]
+    }
+    if partialResult > result {
+        result = partialResult
     }
 }
 
