29603118964715308e6be978714249cb91df9ed1
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-05-12 16:47:46 +0900

    Create 퇴사.swift

---
+++ Gundy/BAEKJOON/다이나믹 프로그래밍/퇴사.swift
@@ -0,0 +1,23 @@
+let days = Int(readLine()!)!
+let quests: [[Int]] = {
+    var quests = [[Int]]()
+
+    for _ in 1...days {
+        quests.append(readLine()!.split(separator: " ").compactMap({ Int($0) }))
+    }
+
+    return quests
+}()
+var points = [Int: Set<Int>]()
+
+for currentDay in 0..<days {
+    let dDay = currentDay + quests[currentDay][0]
+    let currentPoint = points[currentDay, default: [0]].sorted().last!
+    if dDay <= days {
+        for day in dDay...days {
+            points[day, default: []].insert(currentPoint + quests[currentDay][1])
+        }
+    }
+}
+
+print(points[days, default: [0]].sorted().last!)
