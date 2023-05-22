74337081d62f23f41450327fef8a3e20fcd7d51d
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-05-22 14:29:49 +0900

    Update 스택.swift

--- Gundy/BAEKJOON/스택/스택.swift
+++ Gundy/BAEKJOON/스택/스택.swift
@@ -1,35 +1,37 @@
-var stack: [Int] = []
+struct Stack<T> {
 
-func solution() {
-    let input = readLine()!
-    
-    switch input {
-    case "pop":
-        guard let poppedElement = stack.popLast() else {
-            print(-1)
-            return
-        }
-        print(poppedElement)
-    case "size":
-        print(stack.count)
-    case "empty":
-        let value: Int = stack.isEmpty ? 1 : 0
-        print(value)
-    case "top":
-        guard let last = stack.last else {
-            print(-1)
-            return
-        }
-        print(last)
-    default:
-        var text = input
-        text.removeFirst(5)
-        stack.append(Int(text)!)
+    private var container: [T]
+    var last: T? { return container.last }
+    var count: Int { return container.count }
+    var isEmpty: Bool { return container.isEmpty }
+
+    init(_ elements: [T] = []) {
+        self.container = elements
+    }
+
+    mutating func push(_ newElemnet: T) {
+        container.append(newElemnet)
+    }
+
+    mutating func pop() -> T? {
+        return container.popLast()
     }
 }
 
-let count = Int(readLine()!)!
+var stack = Stack([Int]())
 
-for _ in 1...count {
-    solution()
+for _ in 1...Int(readLine()!)! {
+    let command = readLine()!
+    switch command {
+    case "pop":
+        print(stack.pop() ?? -1)
+    case "size":
+        print(stack.count)
+    case "empty":
+        print(stack.isEmpty ? 1 : 0)
+    case "top":
+        print(stack.last ?? -1)
+    default:
+        stack.push(command.split(separator: " ").compactMap({ Int($0) })[0])
+    }
 }
