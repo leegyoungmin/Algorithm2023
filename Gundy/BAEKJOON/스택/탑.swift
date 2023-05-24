8ee1e9ef678701f9d25888c7e365ef097aa92e27
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-05-24 11:08:18 +0900

    Update 탑.swift

--- Gundy/BAEKJOON/스택/탑.swift
+++ Gundy/BAEKJOON/스택/탑.swift
@@ -1,22 +1,15 @@
-func solution() {
-    let _ = readLine()
-    var towers: [Int] = readLine()!.split(separator: " ").compactMap({ Int($0) })
-    var result: [String] = Array(repeating: "0", count: towers.count)
-    var stack: [(Int, Int)] = []
-    var index = towers.count
-    while towers.isEmpty == false {
-        index -= 1
-        let value = towers.removeLast()
-        while stack.isEmpty == false {
-            if stack.last!.0 < value {
-                result[stack.removeLast().1] = String(index + 1)
-            } else {
-                break
-            }
-        }
-        stack.append((value, index))
+let numberOfTowers = Int(readLine()!)!
+let towers = readLine()!.split(separator: " ").compactMap({ Int($0) })
+var result = Array(repeating: "0", count: numberOfTowers)
+var stack = [(tower: Int, index: Int)]()
+
+for (index, tower) in towers.enumerated().reversed() {
+    while stack.isEmpty == false,
+          stack.last!.tower < tower {
+        let last = stack.removeLast()
+        result[last.index] = String(index + 1)
     }
-    print(result.joined(separator: " "))
+    stack.append((tower, index))
 }
 
-solution()
+print(result.joined(separator: " "))
