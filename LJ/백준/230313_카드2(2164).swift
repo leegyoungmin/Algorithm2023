0f566d270e6c8d0a4ec051b1b2337f4a850d1bc4
LJ <swtw72@gmail.com>
2023-05-25 11:34:58 +0900

    Create 230313_카드2(2164).swift

---
+++ LJ/백준/230313_카드2(2164).swift
@@ -0,0 +1,28 @@
+// 카드2(2164)
+// https://www.acmicpc.net/problem/2164
+
+let n = Int(readLine()!)!
+var cards = [Int]()
+
+for number in 1...n {
+    cards.append(number)
+}
+
+let lastCard = solution()
+print(lastCard)
+
+func solution() -> Int {
+    var a: Int = 0
+    var b: Int = 1
+
+    if cards.count == 1 {
+        return cards[0]
+    }
+    while cards[a] != cards[b] {
+        cards.append(cards[b])
+        a += 2
+        b = a+1
+        if b == cards.count { break }
+    }
+    return cards[a]
+}
