8044661802a630ed6ff746da1ac25ad82aad583d
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-05-26 15:06:16 +0900

    Update 카드2.swift

--- Gundy/BAEKJOON/큐/카드2.swift
+++ Gundy/BAEKJOON/큐/카드2.swift
@@ -1,45 +1,12 @@
-final class Node {
-    let value: Int
-    var next: Node?
-    
-    init(_ value: Int) {
-        self.value = value
+var cards = Array(1...Int(readLine()!)!)
+var index = 0
+
+while index < cards.count {
+    index += 1
+    if index < cards.count {
+        cards.append(cards[index])
+        index += 1
     }
 }
 
-struct Queue {
-    var first: Node?
-    var last: Node?
-    
-    mutating func enqueue(_ value: Int) {
-        if first == nil {
-            first = Node(value)
-            last = first
-        } else {
-            last?.next = Node(value)
-            last = last?.next
-        }
-    }
-    
-    @discardableResult mutating func dequeue() -> Int? {
-        let node = first
-        first = first?.next
-        return node?.value
-    }
-}
-
-func solution() {
-    let size: Int = Int(readLine()!)!
-    var numbers = Queue()
-    for number in 1...size {
-        numbers.enqueue(number)
-    }
-    for _ in 1..<size {
-        numbers.dequeue()
-        let number = numbers.dequeue()!
-        numbers.enqueue(number)
-    }
-    print(numbers.dequeue()!)
-}
-
-solution()
+print(cards[cards.count - 1])
