82939053f2b9b5206a3587f1db3363de3d1cbdae
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-05-28 15:09:50 +0900

    Update 이항 계수 2.swift

--- Gundy/BAEKJOON/수학/이항 계수 2.swift
+++ Gundy/BAEKJOON/수학/이항 계수 2.swift
@@ -1,21 +1,12 @@
-let input = readLine()!.split(separator: " ").compactMap({ Int($0) })
-var binomialCoefficients = Array(repeating: Array(repeating: 0, count: input[0] + 1), count: input[0] + 1)
+let numbers = readLine()!.split(separator: " ").compactMap({ Int($0) })
+var dp = Array(repeating: Array(repeating: 1, count: numbers[0] + 1), count: numbers[0] + 1)
 
-binomialCoefficients[0][0] = 1
-
-if input[0] > 0 {
-    for n in 1...input[0] {
-        binomialCoefficients[n][0] = 1
-        binomialCoefficients[n][n] = 1
-    }
-}
-
-if input[0] > 1 {
-    for n in 2...input[0] {
+if numbers[0] > 1 {
+    for n in 2...numbers[0] {
         for k in 1..<n {
-            binomialCoefficients[n][k] = (binomialCoefficients[n - 1][k] + binomialCoefficients[n - 1][k - 1]) % 10007
+            dp[n][k] = (dp[n-1][k] + dp[n-1][k-1]) % 10007
         }
     }
 }
 
-print(binomialCoefficients[input[0]][input[1]])
+print(dp[numbers[0]][numbers[1]])
