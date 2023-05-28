0095a47264269e30ca8b8f7305981ba96756e2bb
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-05-28 15:25:48 +0900

    Update 수 찾기.swift

--- Gundy/BAEKJOON/이분탐색/수 찾기.swift
+++ Gundy/BAEKJOON/이분탐색/수 찾기.swift
@@ -1,38 +1,25 @@
 let _ = readLine()
 let numbers = readLine()!.split(separator: " ").compactMap({ Int($0) }).sorted()
 let _ = readLine()
-let checkList = readLine()!.split(separator: " ").compactMap({ Int($0) })
+let result = readLine()!.split(separator: " ").compactMap({ Int($0) }).map({ isContains($0) ? 1 : 0 })
 
-for number in checkList {
-    var startIndex = 0
-    var endIndex = numbers.count - 1
-    var isPrinted = false
+for number in result {
+    print(number)
+}
 
-    guard number >= numbers.first!,
-          number <= numbers.last! else {
-        print(0)
-        continue
-    }
-
-    binarySearching: while startIndex < endIndex - 1 {
-        let halfIndex = (startIndex + endIndex) / 2
-        switch numbers[halfIndex] {
+func isContains(_ number: Int) -> Bool {
+    var start = 0
+    var end = numbers.count - 1
+    while start <= end {
+        let half = (start + end) / 2
+        switch numbers[half] {
         case ..<number:
-            startIndex = halfIndex
+            start = half + 1
         case number:
-            print(1)
-            isPrinted = true
-            break binarySearching
+            return true
         default:
-            endIndex = halfIndex
+            end = half - 1
         }
     }
-
-    guard isPrinted == false else { continue }
-
-    if numbers[startIndex] == number || numbers[endIndex] == number {
-        print(1)
-    } else {
-        print(0)
-    }
+    return false
 }
