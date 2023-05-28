4599fdedf8cd6af4c8065dbc958de562862222bd
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-05-28 14:06:48 +0900

    Update 종이의 개수.swift

--- Gundy/BAEKJOON/재귀/종이의 개수.swift
+++ Gundy/BAEKJOON/재귀/종이의 개수.swift
@@ -1,38 +1,40 @@
-let length: Int = Int(readLine()!)!
-var board: [[String]] = []
-for _ in 1...length {
-    board.append(readLine()!.split(separator: " ").map({ String($0) }))
+var papers = [[Int]]()
+
+for _ in 1...Int(readLine()!)! {
+    papers.append(readLine()!.split(separator: " ").compactMap({ Int($0) }))
 }
-let result = recursion(board.count / 3, originPoint: (0, 0))
-print(result.negative)
-print(result.zero)
-print(result.positive)
-func recursion(_ term: Int, originPoint: (row: Int, column: Int)) -> (negative: Int, zero: Int, positive: Int) {
-    if term == 0 {
-        switch board[originPoint.row][originPoint.column] {
-        case "-1":
+
+let result = recursion(x: 0, y: 0, size: papers.count)
+print(result.negative, result.zero, result.positive, separator: "\n")
+
+func recursion(x: Int, y: Int, size: Int) -> (negative: Int, zero: Int, positive: Int) {
+    guard size > 1 else {
+        switch papers[x][y] {
+        case -1:
             return (1, 0, 0)
-        case "0":
+        case 0:
             return (0, 1, 0)
         default:
             return (0, 0, 1)
         }
     }
-    let origin = recursion(term / 3, originPoint: originPoint)
-    var isEqual: Bool = origin.negative + origin.zero + origin.positive == 1 ? true : false
-    var negative: Int = origin.negative
-    var zero: Int = origin.zero
-    var positive: Int = origin.positive
-    for (row, column) in zip([0, 0, term, term, term, term * 2, term * 2, term * 2],
-                             [term, term * 2, 0, term, term * 2, 0, term, term * 2]) {
-        let temporary = recursion(term / 3, originPoint: (originPoint.row + row, originPoint.column + column))
-        if isEqual,
-           origin != temporary {
-            isEqual = false
-        }
-        negative += temporary.negative
-        zero += temporary.zero
-        positive += temporary.positive
+    let xPoints = [x, x + size / 3, x + size / 3 * 2,
+                   x, x + size / 3, x + size / 3 * 2,
+                   x, x + size / 3, x + size / 3 * 2]
+    let yPoints = [y, y, y,
+                   y + size / 3, y + size / 3, y + size / 3,
+                   y + size / 3 * 2, y + size / 3 * 2, y + size / 3 * 2]
+    let result = zip(xPoints, yPoints).map({ recursion(x: $0.0, y: $0.1, size: size / 3) })
+    if result[0].negative + result[0].zero + result[0].positive == 1,
+       result.filter({ result[0] == $0 }).count == result.count {
+        return result[0]
+    } else {
+        var partialResult = (0, 0, 0)
+        result.forEach({
+            partialResult.0 += $0.negative
+            partialResult.1 += $0.zero
+            partialResult.2 += $0.positive
+        })
+        return partialResult
     }
-    return isEqual ? origin : (negative, zero, positive)
 }
