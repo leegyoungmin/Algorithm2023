1d1c4daab094deb144fa5d341e968984e4d65621
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-05-28 16:10:01 +0900

    Update 세 수의 합.swift

--- Gundy/BAEKJOON/이분탐색/세 수의 합.swift
+++ Gundy/BAEKJOON/이분탐색/세 수의 합.swift
@@ -6,23 +6,40 @@ for _ in 1...Int(readLine()!)! {
 
 numbers.sort()
 
-var double = Set<Int>()
+var double = [Int]()
 
 for first in 0..<numbers.count - 1 {
     for second in first..<numbers.count - 1 {
-        double.insert(numbers[first] + numbers[second])
+        double.append(numbers[first] + numbers[second])
     }
 }
 
-solution()
+double.sort()
 
-func solution() {
-    for first in stride(from: numbers.count - 1, through: 1, by: -1) {
-        for second in 0..<first {
-            if double.contains(numbers[first] - numbers[second]) {
-                print(numbers[first])
-                return
-            }
+var result = 0
+
+for first in 0..<numbers.count {
+    for second in 0...first {
+        if firstIndex(of: numbers[first] - numbers[second]) != nil,
+           result < numbers[first] {
+            result = numbers[first]
         }
     }
+}
+
+print(result)
+
+func firstIndex(of number: Int) -> Int? {
+    var start = 0
+    var end = double.count - 1
+    while start <= end {
+        let half = (start + end) / 2
+        if double[half] >= number {
+            end = half - 1
+        } else {
+            start = half + 1
+        }
+    }
+    guard start < double.count else { return nil }
+    return double[start] == number ? start : nil
 }
