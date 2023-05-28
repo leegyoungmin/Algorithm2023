58900caecf09e20c9094a251dcd8e3ab82cf997e
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-05-28 16:09:51 +0900

    Update 좌표 압축.swift

--- Gundy/BAEKJOON/이분탐색/좌표 압축.swift
+++ Gundy/BAEKJOON/이분탐색/좌표 압축.swift
@@ -1,34 +1,21 @@
 let _ = readLine()
-let numbers = readLine()!.split(separator: " ").compactMap({ Int($0) })
-let sortedNumbers = Set(numbers).sorted()
-var result = numbers.compactMap({ findFirstIndex(of: $0) })
+let targetNumbers = readLine()!.split(separator: " ").compactMap({ Int($0) })
+let numbers = Set(targetNumbers).sorted()
+let result = targetNumbers.compactMap({ firstIndex(of: $0) })
 
-print(result.map({ "\($0)" }).joined(separator: " "))
+print(result.map({ String($0) }).joined(separator: " "))
 
-func findFirstIndex(of number: Int) -> Int {
-    var startIndex = 0
-    var endIndex = sortedNumbers.count - 1
-    var targetIndex = -1
-
-    while targetIndex == -1,
-          endIndex - startIndex > 1 {
-        let halfIndex = (startIndex + endIndex) / 2
-        switch sortedNumbers[halfIndex] {
-        case ..<number:
-            startIndex = halfIndex
-        case number:
-            targetIndex = halfIndex
-        default:
-            endIndex = halfIndex
+func firstIndex(of number: Int) -> Int? {
+    var start = 0
+    var end = numbers.count - 1
+    while start <= end {
+        let half = (start + end) / 2
+        if numbers[half] >= number {
+            end = half - 1
+        } else {
+            start = half + 1
         }
     }
-    guard targetIndex == -1 else { return targetIndex }
-
-    if sortedNumbers[startIndex] == number {
-        targetIndex = startIndex
-    } else if sortedNumbers[endIndex] == number {
-        targetIndex = endIndex
-    }
-
-    return targetIndex
+    guard start < numbers.count else { return nil }
+    return numbers[start] == number ? start : nil
 }
