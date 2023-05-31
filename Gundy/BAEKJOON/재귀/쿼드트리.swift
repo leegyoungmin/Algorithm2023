a020d78d75bc7613a709352ce25a835699083a8c
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-05-31 16:43:27 +0900

    Update 쿼드트리.swift

--- Gundy/BAEKJOON/재귀/쿼드트리.swift
+++ Gundy/BAEKJOON/재귀/쿼드트리.swift
@@ -1,27 +1,22 @@
-func solution() {
-    let length = Int(readLine()!)!
-    var scene = [[Int]]()
-    for _ in 1...length {
-        scene.append(readLine()!.compactMap({ Int(String($0)) }))
-    }
-    func recursion(term: Int, origin point: (row: Int, column: Int)) -> String {
-        guard term > 0 else { return "\(scene[point.row][point.column])" }
-        let origin = recursion(term: term / 2, origin: point)
-        var result = [origin]
-        var isEqual = origin.count == 1 ? true : false
-        for (row, column) in zip([point.row, point.row + term, point.row + term],
-                                 [point.column + term, point.column, point.column + term]) {
-            let otherPoint = recursion(term: term / 2, origin: (row, column))
-            if isEqual,
-               origin != otherPoint {
-                isEqual = false
-            }
-            result.append(otherPoint)
-        }
-        return isEqual ? origin : result.map({ $0.count > 1 ? "(\($0))" : "\($0)" }).joined()
-    }
-    let result = recursion(term: length / 2, origin: (0, 0))
-    print(result.count == 1 ? "\(result)" : "(\(result))")
+var image = [[String]]()
+
+for _ in 1...Int(readLine()!)! {
+    image.append(readLine()!.map({ String($0) }))
 }
 
-solution()
+print(recursion(origin: (0, 0), size: image.count))
+
+func recursion(origin point: (x: Int, y: Int), size: Int) -> String {
+    guard size > 1 else { return image[point.x][point.y] }
+
+    let xPoints = [point.x, point.x, point.x + size / 2, point.x + size / 2]
+    let yPoints = [point.y, point.y + size / 2, point.y, point.y + size / 2]
+    let result = zip(xPoints, yPoints).map({ recursion(origin: ($0, $1), size: size / 2) })
+
+    if result[0].count == 1,
+       result.filter({ result[0] == $0 }).count == 4 {
+        return result[0]
+    } else {
+        return "(" + result.joined() + ")"
+    }
+}
