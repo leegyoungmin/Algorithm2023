0088e8ae51c2cea7db5c7bf1934d4625d9a0018e
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-06-02 16:23:16 +0900

    Create 무한 수열.swift

---
+++ Gundy/BAEKJOON/해시/무한 수열.swift
@@ -0,0 +1,16 @@
+var register = [Int: Int]()
+register[0] = 1
+
+let input = readLine()!.split(separator: " ").compactMap {
+    return Int($0)
+}
+
+func recursion(_ index: Int) -> Int {
+    guard let number = register[index] else {
+        register[index] = recursion(index / input[1]) + recursion(index / input[2])
+        return register[index]!
+    }
+    return number
+}
+
+print(recursion(input[0]))
