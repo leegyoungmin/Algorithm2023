66be43cf37bf90d77b8fa4e9c6fe7a6d311580bb
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-06-09 16:48:13 +0900

    Update 타겟 넘버.swift

--- Gundy/Programmers/LV. 2/타겟 넘버.swift
+++ Gundy/Programmers/LV. 2/타겟 넘버.swift
@@ -1,14 +1,15 @@
 func solution(_ numbers:[Int], _ target:Int) -> Int {
-    var result: [Int] = []
-    var needVisit: [(partialResult: Int, index: Int)] = [(numbers[0], 0), (-1 * numbers[0], 0)]
-    while needVisit.isEmpty == false {
-        let (number, index) = needVisit.removeLast()
-        guard index < numbers.count - 1 else {
-            result.append(number)
-            continue
+    var result = 0
+    func recursion(_ partialResult: Int, _ index: Int) {
+        guard index < numbers.count else {
+            if partialResult == target {
+                result += 1
+            }
+            return
         }
-        needVisit.append((number + numbers[index + 1], index + 1))
-        needVisit.append((number - numbers[index + 1], index + 1))
+        recursion(partialResult + numbers[index], index + 1)
+        recursion(partialResult - numbers[index], index + 1)
     }
-    return result.filter({ $0 == target }).count
+    recursion(0, 0)
+    return result
 }
