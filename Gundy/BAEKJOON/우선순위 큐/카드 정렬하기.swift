95e537c51c178bbfca895bc0bd668a33551bf466
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-06-12 22:00:57 +0900

    Create 카드 정렬하기.swift

---
+++ Gundy/BAEKJOON/우선순위 큐/카드 정렬하기.swift
@@ -0,0 +1,93 @@
+struct Heap<Element> {
+    private var container: [Element]
+    var isEmpty: Bool {
+        return container.isEmpty
+    }
+    var count: Int {
+        return container.count
+    }
+    private var condition: (Element, Element) -> Bool
+
+    init(_ elements: [Element] = [], condition: @escaping (Element, Element) -> Bool) {
+        container = elements
+        self.condition = condition
+    }
+
+    mutating func insert(_ newElement: Element) {
+        container.append(newElement)
+
+        var index = count - 1
+        var parentIndex = getParentIndex(of: index)
+
+        while index > 0,
+              condition(container[index], container[parentIndex]) {
+            container.swapAt(index, parentIndex)
+            index = parentIndex
+            parentIndex = getParentIndex(of: index)
+        }
+    }
+
+    mutating func remove() -> Element? {
+        guard isEmpty == false else {
+            return nil
+        }
+
+        container.swapAt(0, container.count - 1)
+
+        let element = container.removeLast()
+        var index = 0
+
+        while index < count {
+            let leftChildIndex = getLeftChildIndex(of: index)
+            let rightChildIndex = getRightChildIndex(of: index)
+            var parentIndex = index
+
+            if leftChildIndex < count,
+               condition(container[leftChildIndex], container[parentIndex]) {
+                parentIndex = leftChildIndex
+            }
+
+            if rightChildIndex < count,
+               condition(container[rightChildIndex], container[parentIndex]) {
+                parentIndex = rightChildIndex
+            }
+
+            guard index != parentIndex else {
+                break
+            }
+
+            container.swapAt(index, parentIndex)
+            index = parentIndex
+        }
+        return element
+    }
+
+    private func getParentIndex(of index: Int) -> Int {
+        return (index - 1) / 2
+    }
+
+    private func getLeftChildIndex(of index: Int) -> Int {
+        return index * 2 + 1
+    }
+
+    private func getRightChildIndex(of index: Int) -> Int {
+        return index * 2 + 2
+    }
+}
+
+var minimumHeap = Heap<(Int)>(condition: <)
+
+for _ in 1...Int(readLine()!)! {
+    minimumHeap.insert(Int(readLine()!)!)
+}
+
+var result = 0
+
+while minimumHeap.count > 1 {
+    var mergedCards = minimumHeap.remove()!
+    mergedCards += minimumHeap.remove()!
+    result += mergedCards
+    minimumHeap.insert(mergedCards)
+}
+
+print(result)
