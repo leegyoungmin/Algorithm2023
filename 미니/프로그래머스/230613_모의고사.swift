f955e135318cb5cfc90b79fe85fb1b598a91a21c
미니 <52390923+leegyoungmin@users.noreply.github.com>
2023-06-13 23:00:45 +0900

    Update 230613_모의고사.swift

--- 미니/프로그래머스/230613_모의고사.swift
+++ 미니/프로그래머스/230613_모의고사.swift
@@ -1,1 +1,28 @@
+func solution(_ answers:[Int]) -> [Int] {
+    let first = [1,2,3,4,5]
+    let second = [2,1,2,3,2,4,2,5]
+    let third = [3,3,1,1,2,2,4,4,5,5]
+    
+    var result: [Int] = Array(repeating: 0, count: 3)
+    
+    for i in 0..<answers.count {
+        if first[i % 5] == answers[i] {
+            result[0] += 1
+        }
+        
+        if second[i % 8] == answers[i] {
+            result[1] += 1
+        }
+        
+        if third[i % 10] == answers[i] {
+            result[2] += 1
+        }
+    }
+    
+    let maxValue = result.max()!
+    
+    return result.enumerated().filter { $0.element == maxValue }.map { $0.offset + 1 }.sorted()
+}
 
+let answers = [1,3,2,4,2]
+print(solution(answers))
