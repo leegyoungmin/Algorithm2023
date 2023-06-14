73e4c49e66bc063854d1e5fb3d44f98ae2c677f8
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-06-14 15:37:09 +0900

    Update 모의고사.swift

--- Gundy/Programmers/LV. 1/모의고사.swift
+++ Gundy/Programmers/LV. 1/모의고사.swift
@@ -1,19 +1,35 @@
-func solution(_ answers:[Int]) -> [Int] {
-    let firstPoint: (value: Int, index: Int) = (compare(answers, and: [1, 2, 3, 4, 5]), 1)
-    let secondPoint: (value: Int, index: Int) = (compare(answers, and: [2, 1, 2, 3, 2, 4, 2, 5]), 2)
-    let thirdPoint: (value: Int, index: Int) = (compare(answers, and: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]), 3)
-    let maximumPoint: Int = max(max(firstPoint.value, secondPoint.value), thirdPoint.value)
-    return [firstPoint, secondPoint, thirdPoint].filter({ $0.value == maximumPoint }).map({ $0.index })
-}
+enum MathGiver: CaseIterable {
+    case equally
+    case twoMainly
+    case eachNumberTwice
 
-func compare(_ answers: [Int], and numbers: [Int]) -> Int {
-    var result: Int = 0
-    for index in 0..<answers.count {
-        let answer: Int = answers[index]
-        let number: Int = numbers[index % numbers.count]
-        if answer == number {
-            result += 1
+    var pick: [Int] {
+        switch self {
+        case .equally:
+            return Array(1...5)
+        case .twoMainly:
+            return [1, 3, 4, 5].flatMap { [2, $0] }
+        case .eachNumberTwice:
+            return [3, 1, 2, 4, 5].flatMap { [$0, $0] }
         }
     }
-    return result
+}
+
+func solution(_ answers:[Int]) -> [Int] {
+    var maxCount = 0
+    var result = [(Int, Int)]()
+    for mathGiver in MathGiver.allCases {
+        let pick = mathGiver.pick
+        var count = 0
+        for index in 0..<answers.count {
+            if answers[index] == pick[index % pick.count] {
+                count += 1
+            }
+        }
+        if count > maxCount {
+            maxCount = count
+        }
+        result.append((result.count + 1, count))
+    }
+    return result.filter { $0.1 == maxCount }.map{ $0.0 }
 }
