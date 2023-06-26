0e3c138405a304fc346874e28263f3bb09f79b3b
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-06-26 16:17:56 +0900

    Update 영어 끝말잇기.swift

--- zhilly/Programmers/영어 끝말잇기.swift
+++ zhilly/Programmers/영어 끝말잇기.swift
@@ -22,11 +22,3 @@ func solution(_ n:Int, _ words:[String]) -> [Int] {
     
     return [0, 0]
 }
-
-func solution(_ n:Int, _ left:Int64, _ right:Int64) -> [Int] {
-    let result: [Int] = (Int(left)...Int(right)).map {
-        max($0 / n + 1, $0 % n + 1)
-    }
-    
-    return result
-}
