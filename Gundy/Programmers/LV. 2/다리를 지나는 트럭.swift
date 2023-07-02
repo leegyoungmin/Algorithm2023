2da8fa5ed38a9ba78012645fb6d6bbbf8de4d01b
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-07-02 19:05:10 +0900

    Update 다리를 지나는 트럭.swift

--- Gundy/Programmers/LV. 2/다리를 지나는 트럭.swift
+++ Gundy/Programmers/LV. 2/다리를 지나는 트럭.swift
@@ -1,47 +1,39 @@
 struct Queue {
-    private var left: [Int] = []
-    private var right: [Int] = []
+    private var output: [Int] = []
+    private var input: [Int]
     var value: Int  = 0
     var numberOfDequeues: Int = 0
-    
-    init(_ elements: [Int]) {
-        right = elements
+    var count: Int {
+        return output.count + input.count
     }
-    
+
+    init(_ elements: [Int]) {
+        input = elements
+    }
+
     mutating func enqueue(_ element: Int) {
         value += element
-        right.append(element)
+        input.append(element)
     }
-    
+
     mutating func dequeue() {
         numberOfDequeues += 1
-        if left.isEmpty {
-            left = right.reversed()
-            right = []
+        if output.isEmpty {
+            output = input.reversed()
+            input.removeAll()
         }
-        value -= left.removeLast()
+        value -= output.removeLast()
     }
 }
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
-    var queue: Queue = Queue(Array(repeating: 0, count: bridge_length))
+    var queue = Queue(Array(repeating: 0, count: bridge_length))
     for truck in truck_weights {
-        if queue.value + truck <= weight {
-            queue.dequeue()
-            queue.enqueue(truck)
-        } else {
-            while true {
-                queue.dequeue()
-                if queue.value + truck <= weight {
-                    queue.enqueue(truck)
-                    break
-                } else {
-                    queue.enqueue(0)
-                }
-            }
-        }
-    }
-    while queue.value > 0 {
         queue.dequeue()
+        while queue.value + truck > weight {
+            queue.enqueue(0)
+            queue.dequeue()
+        }
+        queue.enqueue(truck)
     }
-    return queue.numberOfDequeues
+    return queue.numberOfDequeues + queue.count
 }
