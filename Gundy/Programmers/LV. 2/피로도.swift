29ddbb70a0a8948f79eda2ada94043b9644ba752
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-07-06 11:52:03 +0900

    Update 피로도.swift

--- Gundy/Programmers/LV. 2/피로도.swift
+++ Gundy/Programmers/LV. 2/피로도.swift
@@ -1,21 +1,18 @@
 func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
-    var result: [Int] = []
-    var needVisit: [(Int, [Int], Int)] = []
-    for index in 0..<dungeons.count {
-        needVisit.append((k, [index], 1))
-    }
-    while needVisit.isEmpty == false {
-        let (fatigue, indexList, count) = needVisit.removeLast()
-        let dungeon = dungeons[indexList.last!]
-        guard fatigue >= dungeon[0] else {
-            continue
-        }
-        result.append(count)
-        let newFatigue: Int = fatigue - dungeon[1]
+    var result = 0
+    func recursion(current fatigue: Int, visited indices: [Int]) {
         for index in 0..<dungeons.count {
-            guard indexList.contains(index) == false else { continue }
-            needVisit.append((newFatigue, indexList + [index], count + 1))
+            if result < indices.count {
+                result = indices.count
+            }
+            guard indices.contains(index) == false else {
+                continue
+            }
+            if dungeons[index][0] <= fatigue {
+                recursion(current: fatigue - dungeons[index][1], visited: indices + [index])
+            }
         }
     }
-    return result.sorted(by: >).first!
+    recursion(current: k, visited: [])
+    return result
 }
