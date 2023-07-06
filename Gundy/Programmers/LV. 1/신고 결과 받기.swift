7cc29c639ef893998bb599ae0f75f2c3e8ef7099
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-07-06 12:19:25 +0900

    Update 신고 결과 받기.swift

--- Gundy/Programmers/LV. 1/신고 결과 받기.swift
+++ Gundy/Programmers/LV. 1/신고 결과 받기.swift
@@ -1,14 +1,14 @@
 func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
-    var idIndex: [String : Int] = [:]
+    var idIndex = [String : Int]()
     for id in id_list {
         idIndex[id] = idIndex.count
     }
-    var count: [String : Int] = [:]
-    var blacklist: Set<String> = []
-    var reports: [Set<String>] = Array(repeating: [], count: id_list.count)
+    var count = [String: Int]()
+    var blacklist = Set<String>()
+    var reports = Array(repeating: Set<String>(), count: id_list.count)
     report.forEach {
-        let users: [String] = $0.split(separator: " ").map({ String($0) })
-        let index: Int = idIndex[users[0]]!
+        let users = $0.split(separator: " ").map(String.initㅋ)
+        let index = idIndex[users[0]]!
         reports[index].insert(users[1])
     }
     for report in reports {
